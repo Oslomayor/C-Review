b2faa5b3591eaf33a4891a8742eaf745fa2367b9
Denis <34310801+Oslomayor@users.noreply.github.com>
2018-08-06 15:41:16 +0800

    二分法实现字符串反转

---
+++ 二分法使字符串反转.c
@@ -0,0 +1,26 @@
+/* 二分法实现字符串反转 */
+
+#include <stdio.h>
+
+/* 入口参数： 字符串地址，左下标，右下标*/
+/* 返回参数： 无                        */
+void reverse(char s[], int left, int right)
+{
+	int i;
+    char c;
+	for(i=0;i<=(right-left)/2;i++){
+		c = s[right-i];
+        s[right-i] = s[left+i];
+        s[left+i] = c;
+    }
+}
+ 
+int main()
+{
+	char s[10] = "abcdefghi";
+    printf("%s\n",s);
+    reverse(s,0,8);
+    printf("%s\n",s);
+	system("pause");
+	return 0;
+}
